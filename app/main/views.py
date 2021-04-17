06ab22150052220ddd07b7aa8983936af1e2c8db
Ken Thumi <kenthumi@gmail.com>
2021-04-17 05:31:20 +0300

    Added Standard News Link

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,12 @@ def index():
     articles = get_articles('aljazeera.com')
 
     return render_template('index.html',article_list=articles)
+
+
+@main.route('/sources/<source>')
+def source(source):
+    '''Routes you depending on the route source'''
+
+    articles = get_articles(source)
+
+    return render_template('news.html',article_list=articles)
